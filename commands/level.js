f620578ed5ef9cb40847964fa4ee5739b069f6c6
oktayyavuz <76921027+oktayyavuz@users.noreply.github.com>
2024-05-20 14:38:57 +0300

    Rimuru Bot v2.4
    Rimuru Bot v2.4 'Aldığınız hataları iletisim.oktaydev.com.tr Adresinden form atarak iletebilirsiniz.'

--- commands/level.js
+++ commands/level.js
@@ -2,20 +2,31 @@ const { AttachmentBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   name: "level",
-  description: "Seviyenizi görüntüleyin.",
+  description: "Bir kullanıcının seviyesini görüntüleyin.",
   type: 1,
-  options: [],
+  options: [
+    {
+      name: "kullanıcı",
+      description: "Seviyesini görüntülemek istediğiniz kullanıcıyı etiketleyin.",
+      type: 6,
+      required: false,
+    },
+  ],
 
   run: async (client, interaction, db, Rank) => {
     const { user, guild } = interaction;
+    const targetUser = interaction.options.getUser("kullanıcı") || user;
 
-    const level = db.get(`levelPos_${user.id}${guild.id}`) || 0;
-    const xp = db.get(`xpPos_${user.id}${guild.id}`) || 0;
+    const level = db.get(`levelPos_${targetUser.id}${guild.id}`) || 0;
+    const xp = db.get(`xpPos_${targetUser.id}${guild.id}`) || 0;
 
     const embed = new EmbedBuilder()
       .setColor("Random")
-      .setTitle(`@${user.username}'in Seviyesi`)
-      .setDescription(`Seviye: ${level}, XP: ${xp}`);
+      .setTitle(`${targetUser.username} kullanıcısının Seviyesi`)
+      .setDescription(`Seviye: **${level}**\nXP: **${xp}**`)
+      .setThumbnail(targetUser.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setFooter({ text: `${guild.name}`, iconURL: guild.iconURL() });
 
     interaction.reply({ embeds: [embed] });
   },
