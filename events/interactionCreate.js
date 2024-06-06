c75fdea1cc24eedd5c17af1d9f42d37efc9c9169
oktayyavuz <76921027+oktayyavuz@users.noreply.github.com>
2024-06-06 16:03:36 +0300

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -48,71 +48,73 @@ name: Discord.Events.InteractionCreate,
     const idFind = (id_name) => {
       return `${id_name}_${id}`;
     } 
-  
-   if(interaction.customId === 'ticketforms'){
-  const ticketSystem = db.fetch(`ticketSystem_${interaction.guild.id}`)
+  }
 
-
-  const lvl = db.fetch(`ticketLvl_${interaction.guild.id}`) || 0;
-
-  db.add(`ticketLvl_${interaction.guild.id}`, 1)
-
-
-  const ticketYetkili = await interaction.guild.roles.cache.find( ch => ch.id === ticketSystem.yetkili );
-
-  const ticketCategory = db.fetch(`ticketCategory_${interaction.guild.id}`);
-
-  const ticketsebep = interaction.fields.getTextInputValue('ticketInput');
- const channel = await interaction.guild.channels.create({
-   name: `talep-${interaction.user.username}-`+lvl,
-   type: Discord.ChannelType.GuildText,
-   parent: ticketCategory.category,
-   permissionOverwrites: [
-     {
-       id: interaction.guild.id,
-       deny: [Discord.PermissionsBitField.Flags.ViewChannel],
-     },
-      {
-       id: interaction.user.id,
-       allow: [Discord.PermissionsBitField.Flags.ViewChannel, Discord.PermissionsBitField.Flags.SendMessages],
-     },
-     {
-      id: ticketYetkili.id,
-      allow: [Discord.PermissionsBitField.Flags.ViewChannel, Discord.PermissionsBitField.Flags.SendMessages],
-     },
-   ],
- });
- const sebepTicket = new Discord.EmbedBuilder()
- .setDescription(`Neden talep açtığını öğrenebilir miyiz?\n> \`${ticketsebep}\``)
- const ticketUserEmbed = new Discord.EmbedBuilder()
- .setAuthor({ name: `${interaction.user.username} | Destek açıldı`, iconURL: `${interaction.user.displayAvatarURL({ dynmaic: true })} ` })
- .setThumbnail(interaction.guild.iconURL({ dynmaic: true }))
- .addFields([ 
-  { name: "Destek açan:", value: `${interaction.user}`, inline: true },
-  { name: "Açılış zamanı:", value: `<t:${parseInt(channel.createdTimestamp / 1000)}:R>`, inline: true }
- ])
- .setColor('Green')
- .setFooter({ text: `Oluşturan: ${client.user.tag}`, iconURL: `${client.user.displayAvatarURL({ dynmaic: true })}` })
- .setTimestamp()
- 
- const row = new Discord.ActionRowBuilder()
- .addComponents(
-   new Discord.ButtonBuilder()
-     .setCustomId(`ticketClose_everyone`)
-     .setLabel('Destek kapatılsın.')
-     .setEmoji("🔒")
-     .setStyle(Discord.ButtonStyle.Danger),
- );
- 
-  interaction.reply({ content: `:white_check_mark: **|** Senin için bir tane destek kanalı ${channel} oluşturldu.`, ephemeral: true })
-
-  db.set(`ticketChannelUser_${interaction.guild.id}${channel.id}`, { user: interaction.user.id })
-  db.set(`ticketUser_${interaction.user.id}${interaction.guild.id}`, { whOpen: interaction.user.id, date: Date.now() })
-
-  channel.send({ content: `<@${interaction.user.id}> | ${ticketYetkili}`, embeds: [ticketUserEmbed] })
-  return channel.send({ embeds: [sebepTicket], components: [row]  })
-
-}
+  if(interaction.isModalSubmit()) {
+    if(interaction.customId === 'ticketforms'){
+      const ticketSystem = db.fetch(`ticketSystem_${interaction.guild.id}`)
+    
+    
+      const lvl = db.fetch(`ticketLvl_${interaction.guild.id}`) || 0;
+    
+      db.add(`ticketLvl_${interaction.guild.id}`, 1)
+    
+    
+      const ticketYetkili = await interaction.guild.roles.cache.find( ch => ch.id === ticketSystem.yetkili );
+    
+      const ticketCategory = db.fetch(`ticketCategory_${interaction.guild.id}`);
+    
+      const ticketsebep = interaction.fields.getTextInputValue('ticketInput');
+     const channel = await interaction.guild.channels.create({
+       name: `talep-${interaction.user.username}-`+lvl,
+       type: Discord.ChannelType.GuildText,
+       parent: ticketCategory.category,
+       permissionOverwrites: [
+         {
+           id: interaction.guild.id,
+           deny: [Discord.PermissionsBitField.Flags.ViewChannel],
+         },
+          {
+           id: interaction.user.id,
+           allow: [Discord.PermissionsBitField.Flags.ViewChannel, Discord.PermissionsBitField.Flags.SendMessages],
+         },
+         {
+          id: ticketYetkili.id,
+          allow: [Discord.PermissionsBitField.Flags.ViewChannel, Discord.PermissionsBitField.Flags.SendMessages],
+         },
+       ],
+     });
+     const sebepTicket = new Discord.EmbedBuilder()
+     .setDescription(`Neden talep açtığını öğrenebilir miyiz?\n> \`${ticketsebep}\``)
+     const ticketUserEmbed = new Discord.EmbedBuilder()
+     .setAuthor({ name: `${interaction.user.username} | Destek açıldı`, iconURL: `${interaction.user.displayAvatarURL({ dynmaic: true })} ` })
+     .setThumbnail(interaction.guild.iconURL({ dynmaic: true }))
+     .addFields([ 
+      { name: "Destek açan:", value: `${interaction.user}`, inline: true },
+      { name: "Açılış zamanı:", value: `<t:${parseInt(channel.createdTimestamp / 1000)}:R>`, inline: true }
+     ])
+     .setColor('Green')
+     .setFooter({ text: `Oluşturan: ${client.user.tag}`, iconURL: `${client.user.displayAvatarURL({ dynmaic: true })}` })
+     .setTimestamp()
+     
+     const row = new Discord.ActionRowBuilder()
+     .addComponents(
+       new Discord.ButtonBuilder()
+         .setCustomId(`ticketClose_everyone`)
+         .setLabel('Destek kapatılsın.')
+         .setEmoji("🔒")
+         .setStyle(Discord.ButtonStyle.Secondary),
+     );
+     
+      interaction.reply({ content: `:white_check_mark: **|** Senin için bir tane destek kanalı ${channel} oluşturldu.`, ephemeral: true })
+    
+      db.set(`ticketChannelUser_${interaction.guild.id}${channel.id}`, { user: interaction.user.id })
+      db.set(`ticketUser_${interaction.user.id}${interaction.guild.id}`, { whOpen: interaction.user.id, date: Date.now() })
+    
+      channel.send({ content: `<@${interaction.user.id}> | ${ticketYetkili}`, embeds: [ticketUserEmbed] })
+      return channel.send({ embeds: [sebepTicket], components: [row]  })
+    
+    }
 
 if(interaction.customId === 'giriscikis'){
   const joinMsg = interaction.fields.getTextInputValue('girismesaj')
@@ -1441,92 +1443,92 @@ if(interaction.customId === 'giriscikis'){
             message.delete()
         }
 		
-		        if(interaction.customId === `ticketnasilacilir_everyone`) {
-        const embed = new Discord.EmbedBuilder()
-        .setAuthor({ name: `${config["bot-adi"]} Destek Menüsü`, iconURL: client.user.displayAvatarURL({ dynamic: true })})
-        .setTitle("・Destek talebi nasıl oluşturabilirsin.")
-        .setDescription("**Destek Talebi Oluştur** butonuna tıkladıktan sonra karşına bir form gelecektir. O formu doldurduktan sonra destek talebin başarılı bir şekilde oluşturulacaktır.")
-        .setImage(`https://i.hizliresim.com/gsj3vyw.png`)
-        .setColor('Blue')
-          interaction.reply({ embeds: [embed], ephemeral: true })
-        }
-
-                if(interaction.customId === `ticketolustur_everyone`) {
-          
-          const find = db.fetch(`ticketUser_${interaction.user.id}${interaction.guild.id}`)
-          if(find) {
-            const ticketVar = new Discord.EmbedBuilder()
-            .setDescription(`:x: Zaten bir talebin bulunmakta.`)
-            return interaction.reply({ embeds: [ticketVar], ephemeral: true })
+        if(interaction.customId === `ticketnasilacilir_everyone`) {
+          const embed = new Discord.EmbedBuilder()
+          .setAuthor({ name: "Rimuru Destek Menüsü", iconURL: client.user.displayAvatarURL({ dynamic: true })})
+          .setTitle("・Destek talebi nasıl oluşturabilirsin.")
+          .setDescription("**Destek Talebi Oluştur** butonuna tıkladıktan sonra karşına bir form gelecektir. O formu doldurduktan sonra destek talebin başarılı bir şekilde oluşturulacaktır.")
+          .setImage(`https://cdn.discordapp.com/attachments/1235347548873425027/1235523499582296084/image.png?ex=6634ae6d&is=66335ced&hm=c9b678963889765b82be76868f9f40e5dc6a85425e7badb843d91caa85ac07d3&`)
+          .setColor('Blue')
+            interaction.reply({ embeds: [embed], ephemeral: true })
           }
-
-          const ticketmodal = new Discord.ModalBuilder()
-          .setCustomId('ticketforms')
-          .setTitle('Destek Oluşturma Formu');
-    
-          const ticketInput = new Discord.TextInputBuilder()
-          .setCustomId('ticketInput')
-          .setLabel("Destek Oluşturma Sebebiniz Nedir?")
-          .setRequired(true)  
-          .setStyle(Discord.TextInputStyle.Short);
-    
+  
+                  if(interaction.customId === `ticketolustur_everyone`) {
+            
+            const find = db.fetch(`ticketUser_${interaction.user.id}${interaction.guild.id}`)
+            if(find) {
+              const ticketVar = new Discord.EmbedBuilder()
+              .setDescription(`:x: Zaten bir talebin bulunmakta.`)
+              return interaction.reply({ embeds: [ticketVar], ephemeral: true })
+            }
+  
+            const ticketmodal = new Discord.ModalBuilder()
+            .setCustomId('ticketforms')
+            .setTitle('Destek Oluşturma Formu');
       
-          const afirstActionRow = new Discord.ActionRowBuilder().addComponents(ticketInput);
-    
-          ticketmodal.addComponents(afirstActionRow);
-    
-          await interaction.showModal(ticketmodal);
-          
-        }
+            const ticketInput = new Discord.TextInputBuilder()
+            .setCustomId('ticketInput')
+            .setLabel("Destek Oluşturma Sebebiniz Nedir?")
+            .setRequired(true)  
+            .setStyle(Discord.TextInputStyle.Short);
+      
         
-        if(interaction.customId === `ticketClose_everyone`) {
-          interaction.channel.permissionOverwrites.set([
-            {
-              id: interaction.guild.id,
-              deny: [PermissionsBitField.Flags.ViewChannel],
-            },        
-          ]);
-          const row = new Discord.ActionRowBuilder()
-          .addComponents(
-            new Discord.ButtonBuilder()
-              .setCustomId(`ticketDelete_everyone`)
-              .setLabel('Destek silinsin.')
-              .setEmoji("🗑️")
-              .setStyle(Discord.ButtonStyle.Danger),
-          );
-          const ticketClose = new Discord.EmbedBuilder()
-          .setDescription(`:white_check_mark: | Bu destek talebi kapatılmıştır.`)
-          .setColor('Green')
-          interaction.reply({ embeds: [ticketClose], components: [row] })
-        }
-
-        if(interaction.customId === `ticketDelete_everyone`) {
-
-         const chnl = db.fetch(`ticketChannelUser_${interaction.guild.id}${interaction.channel.id}`);
-         const x = chnl.user;
-
-         const adam = await interaction.guild.members.cache.find(user => user.id === x);
-         const usr = db.fetch(`ticketUser_${x}${interaction.guild.id}`);
-
-          const ticketLog = db.fetch(`ticketKanal_${interaction.guild.id}`)
-          const ticketcloseembed = new EmbedBuilder()
-          .setTitle(`${adam.user.tag} adlı kişinin destek verileri.`)
-          .addFields(
-            { name: "Destek Açan: <:block:1065690244634124500>", value: `<@${usr.whOpen}>`, inline: true },
-            { name: "Desteğin Kapatılış Tarihi:", value: `<t:${parseInt(Date.now() / 1000)}:R>`, inline: true  },
-             { name: '\u200B', value: '\u200B' },
-            { name: "Desteği Kapatan:", value: `<@${interaction.user.id}>`, inline: true },
-            { name: "Desteğin Açılış Tarihi:", value: `<t:${parseInt(usr.date / 1000)}:R>`, inline: true  },
-                  )
-          .setColor('Green')
-          .setThumbnail(`${adam.user.displayAvatarURL()}`)
-          client.channels.cache.get(ticketLog).send({embeds: [ticketcloseembed]})
-
-          db.delete(`ticketChannelUser_${interaction.guild.id}${interaction.channel.id}`);
-          db.delete(`ticketUser_${x}${interaction.guild.id}`);
-
-          return interaction.channel.delete();
-        }
+            const afirstActionRow = new Discord.ActionRowBuilder().addComponents(ticketInput);
+      
+            ticketmodal.addComponents(afirstActionRow);
+      
+            await interaction.showModal(ticketmodal);
+            
+          }
+          
+          if(interaction.customId === `ticketClose_everyone`) {
+            interaction.channel.permissionOverwrites.set([
+              {
+                id: interaction.guild.id,
+                deny: [PermissionsBitField.Flags.ViewChannel],
+              },        
+            ]);
+            const row = new Discord.ActionRowBuilder()
+            .addComponents(
+              new Discord.ButtonBuilder()
+                .setCustomId(`ticketDelete_everyone`)
+                .setLabel('Destek silinsin.')
+                .setEmoji("🗑️")
+                .setStyle(Discord.ButtonStyle.Secondary),
+            );
+            const ticketClose = new Discord.EmbedBuilder()
+            .setDescription(`:white_check_mark: | Bu destek talebi kapatılmıştır.`)
+            .setColor('Green')
+            interaction.reply({ embeds: [ticketClose], components: [row] })
+          }
+  
+          if(interaction.customId === `ticketDelete_everyone`) {
+  
+           const chnl = db.fetch(`ticketChannelUser_${interaction.guild.id}${interaction.channel.id}`);
+           const x = chnl.user;
+  
+           const adam = await interaction.guild.members.cache.find(user => user.id === x);
+           const usr = db.fetch(`ticketUser_${x}${interaction.guild.id}`);
+  
+            const ticketLog = db.fetch(`ticketKanal_${interaction.guild.id}`)
+            const ticketcloseembed = new EmbedBuilder()
+            .setTitle(`${adam.user.tag} adlı kişinin destek verileri.`)
+            .addFields(
+              { name: "Destek Açan: <:block:1065690244634124500>", value: `<@${usr.whOpen}>`, inline: true },
+              { name: "Desteğin Kapatılış Tarihi:", value: `<t:${parseInt(Date.now() / 1000)}:R>`, inline: true  },
+               { name: '\u200B', value: '\u200B' },
+              { name: "Desteği Kapatan:", value: `<@${interaction.user.id}>`, inline: true },
+              { name: "Desteğin Açılış Tarihi:", value: `<t:${parseInt(usr.date / 1000)}:R>`, inline: true  },
+                    )
+            .setColor('Green')
+            .setThumbnail(`${adam.user.displayAvatarURL()}`)
+            client.channels.cache.get(ticketLog).send({embeds: [ticketcloseembed]})
+  
+            db.delete(`ticketChannelUser_${interaction.guild.id}${interaction.channel.id}`);
+            db.delete(`ticketUser_${x}${interaction.guild.id}`);
+  
+            return interaction.channel.delete();
+          }
       
 if(interaction.customId === `benıdogrula_everyone_${interaction.guild.id}${interaction.user.id}`)
         {
