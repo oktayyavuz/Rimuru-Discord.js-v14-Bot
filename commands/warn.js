510be2fb59711fbae10773cb7451dabd60d82d80
oktayyavuz <76921027+oktayyavuz@users.noreply.github.com>
2024-06-20 17:18:18 +0300

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,10 +1,10 @@
 const { EmbedBuilder } = require("discord.js");
 const db = require("croxydb");
 const ms = require("ms");
-const config = require("../config.json"); 
+const config = require("../config.json");
 
 module.exports = {
-  name: "Uyarı",
+  name: "uyarı",
   description: "Uyarı sistemi",
   type: 1,
   options: [
@@ -23,11 +23,16 @@ module.exports = {
   ],
 
   run: async (client, interaction) => {
- const mod = db.get(`Mod_${interaction.guild.id}`);
+    const modRoleID = db.get(`Mod_${interaction.guild.id}`);
+    const modRole = interaction.guild.roles.cache.get(modRoleID);
 
-if (!interaction.member.roles.cache.has(mod.id)) {
-  return interaction.reply({ content: "Bu komutu kullanmak için yetkiniz yok.", ephemeral: false });
-}
+    if (!modRole) {
+      return interaction.reply({ content: "Mod rolü ayarlanmamış. Lütfen önce mod rolünü ayarlayın.", ephemeral: true });
+    }
+
+    if (!interaction.member.roles.cache.has(modRole.id)) {
+      return interaction.reply({ content: "Bu komutu kullanmak için yetkiniz yok.", ephemeral: true });
+    }
 
     const user = interaction.options.getMember("kullanıcı");
     const reason = interaction.options.getString("sebep");
@@ -37,14 +42,15 @@ if (!interaction.member.roles.cache.has(mod.id)) {
     }
 
     const guildKey = `${interaction.guild.id}.${user.id}`;
-    const warningCount = db.get(guildKey) || 1;
+    const warningCount = db.get(guildKey) || 0;
 
     db.add(guildKey, 1);
+    const updatedWarningCount = warningCount + 1;
 
     const embed = new EmbedBuilder()
       .setColor("Aqua")
       .setTitle("Uyarı Bilgisi")
-      .setDescription(`${user} üyesinin uyarı sayısı: ${warningCount}`)
+      .setDescription(`${user} üyesinin uyarı sayısı: ${updatedWarningCount}`)
       .addFields({ name: "Uyarı Sebebi", value: reason })
       .setTimestamp()
       .setFooter({ text: `${config["bot-adi"]}`, iconURL: interaction.guild.iconURL({ format: "png", dynamic: true, size: 2048 }) });
@@ -58,26 +64,45 @@ if (!interaction.member.roles.cache.has(mod.id)) {
       console.error("Log kanalı bulunamadı.");
     }
 
+    const dmEmbed = new EmbedBuilder()
+      .setColor("Aqua")
+      .setTitle(`Uyarı Aldınız`)
+      .setDescription(`## ${interaction.guild.name} Sunucusundan uyarı aldınız\n**Uyarı Sebebi:** ${reason}`);
+
+    try {
+      await user.send({ embeds: [dmEmbed] });
+    } catch (err) {
+      console.error('DM mesajı gönderilemedi:', err);
+    }
+
     const muteRoleID = db.get(`Mute_${interaction.guild.id}`);
     const jailRoleID = db.get(`Jail_${interaction.guild.id}`);
+    const muteRole = interaction.guild.roles.cache.get(muteRoleID);
+    const jailRole = interaction.guild.roles.cache.get(jailRoleID);
 
-    if (warningCount === 3 && muteRoleID) {
-      user.roles.add(muteRoleID);
-      setTimeout(() => user.roles.remove(muteRoleID), ms('2h'));
+    if (updatedWarningCount === 3 && muteRole) {
+      user.roles.add(muteRole).catch(console.error);
+      setTimeout(() => {
+        user.roles.remove(muteRole).catch(console.error);
+      }, ms('2h'));
       interaction.reply({ embeds: [embed.setDescription(`${user} üyesi \`3\` uyarı sayısına ulaştığı için <@&${muteRoleID}> rolü verildi. 2 saat sonra kaldırılacak.`)] }).then((e) => setTimeout(() => { e.delete(); }, 12000));
-    } else if (warningCount === 5 && muteRoleID) {
-      user.roles.add(muteRoleID);
-      setTimeout(() => user.roles.remove(muteRoleID), ms('1d'));
+    } else if (updatedWarningCount === 5 && muteRole) {
+      user.roles.add(muteRole).catch(console.error);
+      setTimeout(() => {
+        user.roles.remove(muteRole).catch(console.error);
+      }, ms('1d'));
       interaction.reply({ embeds: [embed.setDescription(`${user} üyesi \`5\` uyarı sayısına ulaştığı için <@&${muteRoleID}> rolü verildi. 1 gün sonra kaldırılacak.`)] }).then((e) => setTimeout(() => { e.delete(); }, 12000));
-    } else if (warningCount === 7 && jailRoleID) {
-      user.roles.add(jailRoleID);
-      setTimeout(() => user.roles.remove(jailRoleID), ms('4d'));
-      interaction.reply({ embeds: [embed.setDescription(`${user} üyesi \`7\` uyarı sayısına ulaştığı için <@&${jailRoleID}> rolü verildi. 4 gün sonra kaldırılacak.`)] }).then((e) => setTimeout(() => { e.delete(); }, 12000));
-    } else if (warningCount === 9) {
-      interaction.guild.members.ban(user.id, { reason: `Otomatik ceza sistemi` });
+    } else if (updatedWarningCount === 7 && jailRole) {
+      user.roles.add(jailRole).catch(console.error);
+      setTimeout(() => {
+        user.roles.remove(jailRole).catch(console.error);
+      }, ms('1w'));
+      interaction.reply({ embeds: [embed.setDescription(`${user} üyesi \`7\` uyarı sayısına ulaştığı için <@&${jailRoleID}> rolü verildi. 1 hafta sonra kaldırılacak.`)] }).then((e) => setTimeout(() => { e.delete(); }, 12000));
+    } else if (updatedWarningCount === 9) {
+      interaction.guild.members.ban(user.id, { reason: `Otomatik ceza sistemi` }).catch(console.error);
       interaction.reply({ embeds: [embed.setDescription(`${user} üyesi \`9\` uyarı sayısına ulaştığı için banlandı.`)] }).then((e) => setTimeout(() => { e.delete(); }, 12000));
     } else {
-      interaction.reply({ embeds: [embed.setDescription(`${user} üyesine uyarı verildi. Uyarı Sayısı: ${warningCount}`)] }).then((e) => setTimeout(() => { e.delete(); }, 12000));
+      interaction.reply({ embeds: [embed.setDescription(`${user} üyesine uyarı verildi. Uyarı Sayısı: ${updatedWarningCount}`)] }).then((e) => setTimeout(() => { e.delete(); }, 12000));
     }
   }
 };
