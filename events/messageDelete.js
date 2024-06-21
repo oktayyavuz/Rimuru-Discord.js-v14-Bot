7ba9c86ec83445b1c020b3e7b955ebb801af3797
oktayyavuz <76921027+oktayyavuz@users.noreply.github.com>
2024-06-21 18:13:04 +0300

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,24 +1,24 @@
 const db = require("croxydb");
-const { EmbedBuilder, Events } = require("discord.js");
+const { PermissionFlagsBits, EmbedBuilder, Events  } = require("discord.js");
 
-module.exports = {
+module.exports =  {
   name: Events.MessageDelete,
 
-  run: async (client, message) => {
-    let kanal = db.get(`modlogK_${message.guild.id}`);
+  run: async(client, message) => {
+    if (!db.has(`modlogK_${message.guild.id}`)) return;
 
-    if (!message.author || !message.author.tag) return;
-
+    let kanal = db.get(`modlogK_${message.guild.id}`)
+    try {
     const embed = new EmbedBuilder()
-      .setColor("Random")
-      .setDescription(`Yeni bir mesaj silindi!`)
-      .addFields(
-        { name: "**Kullanıcı Tag**", value: message.author.tag, inline: false },
-        { name: "**ID**", value: message.author.id, inline: false },
-        { name: "**Silinen Mesaj**", value: "```" + message.content + "```", inline: false },
-        { name: "**Silindiği Zaman**", value: `<t:${Math.floor(message.createdAt / 1000)}:R>`, inline: true }
-      );
-      
-    client.channels.cache.get(kanal).send({ embeds: [embed] });
+    .setColor("Random")
+    .setDescription(`Yeni bir mesaj silindi!`)
+    .addFields(
+      { name: "**Kullanıcı Tag**", value: message.author.tag, inline: false },
+      { name: "**ID**", value: message.author.id, inline: false  },
+      { name: "**Silinen Mesaj**", value: "```" + message.content + "```", inline: false  },
+      { name: "**Silindiği Zaman**", value: `<t:${parseInt(Date.now() / 1000)}:R>`, inline: true }
+        )
+    client.channels.cache.get(kanal).send({ embeds: [embed] })
+  } catch(err) { }
   }
-};
+}
